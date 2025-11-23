5d84f1ec55fe57cccdc5b895034fcae33d1680fa
kartaguez <kartaguez@gmail.com>
2025-11-23 21:34:12 +0100

    Removed unused import.

--- infra-jpa/src/test/java/com/kartaguez/bluepot/infrastructure/down/repository/jpa/PotRepositoryJpaIT.java
+++ infra-jpa/src/test/java/com/kartaguez/bluepot/infrastructure/down/repository/jpa/PotRepositoryJpaIT.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
